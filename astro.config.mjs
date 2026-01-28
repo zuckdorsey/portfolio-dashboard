14129fb2f8d8a59e5d194c6f6690b989adeae70d
Ababil <ababilmustaqim@proton.me>
2026-01-28 17:24:44 +0700

    Add @astrojs/vercel dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  output: 'server', // atau 'hybrid'
+  adapter: vercel(),
 });
